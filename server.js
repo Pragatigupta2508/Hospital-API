12e288d0905ed00f249ca1dc7db7e513f904d939
Pragati Gupta <pragatigupta02508@gmail.com>
2026-03-11 19:46:47 +0530

    added root route

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Hospital API Running Successfully");
+});
 
 mongoose.connect(process.env.MONGO_URI)
 .then(()=> console.log("MongoDB Connected"))
@@ -22,4 +25,7 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT,()=>{
 console.log(`Server running on port ${PORT}`);
+});
+app.get("/", (req, res) => {
+  res.send("Hospital API Running Successfully");
 });
